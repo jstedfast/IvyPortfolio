a24e703ad17a3c098a5e3941d1c78c33783d6586
Jeffrey Stedfast <jestedfa@microsoft.com>
2022-07-01 15:38:05 -0400

    Don't bother with cookie crumbs anymore, they no longer work

--- IvyPortfolio/YahooFinance.cs
+++ IvyPortfolio/YahooFinance.cs
@@ -24,25 +24,17 @@
 // THE SOFTWARE.
 //
 
-using System;
 using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace IvyPortfolio
 {
 	public class YahooFinance : IFinancialService
 	{
-		static readonly Regex CrumbRegex = new Regex ("CrumbStore\":{\"crumb\":\"(?<crumb>.+?)\"}", RegexOptions.CultureInvariant | RegexOptions.Compiled);
 		static readonly DateTime UnixEpoch = new DateTime (1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
 		HttpClient client;
-		string crumb;
 
 		public YahooFinance ()
 		{
@@ -55,20 +47,21 @@ namespace IvyPortfolio
 
 		public async Task<string> GetStockDescriptionAsync (string symbol, CancellationToken cancellationToken)
 		{
-			const string format = "https://finance.yahoo.com/quote/{0}?p={0}";
-			var requestUri = string.Format (format, symbol);
-
-			var html = await client.GetStringAsync (requestUri).ConfigureAwait (false);
+			var requestUri = $"https://finance.yahoo.com/quote/{symbol}/history";
 			int startIndex, endIndex;
+			string html;
 
-			// extract the cookie crumb
-			var crumbs = CrumbRegex.Matches (html);
+			using (var request = new HttpRequestMessage (HttpMethod.Get, requestUri)) {
+				request.Headers.Add ("Accept-Language", "en-US");
+				request.Headers.Add ("Connection", "keep-alive");
 
-			if (crumbs.Count > 0) {
-				crumb = crumbs[0].Groups["crumb"].Value;
-				crumb = crumb.Replace ("\\u002F", "/");
-			} else {
-				crumb = "xxxxxxxxxxx";
+				using (var response = await client.SendAsync (request, cancellationToken).ConfigureAwait (false)) {
+					html = await response.Content.ReadAsStringAsync (cancellationToken).ConfigureAwait (false);
+
+					if (!response.IsSuccessStatusCode) {
+
+					}
+				}
 			}
 
 			if ((endIndex = html.IndexOf ("(" + symbol + ")", StringComparison.Ordinal)) <= 0) {
@@ -89,32 +82,58 @@ namespace IvyPortfolio
 			return html.Substring (startIndex, endIndex - startIndex).Replace ("&amp;", "&");
 		}
 
+		static long SecondsSinceEpoch (DateTime dt)
+		{
+			return (long) (dt - UnixEpoch).TotalSeconds;
+		}
+
 		public async Task<IStockData> GetStockDataAsync (string symbol, DateTime start, DateTime end, CancellationToken cancellationToken)
 		{
-			const string format = "https://query1.finance.yahoo.com/v7/finance/download/{0}?period1={1}&period2={2}&interval=1d&events=history&crumb={3}";
-			var requestUri = string.Format (format, symbol, (start - UnixEpoch).TotalSeconds, (end - UnixEpoch).TotalSeconds, crumb);
+			const string format = "https://query1.finance.yahoo.com/v7/finance/download/{0}?period1={1}&period2={2}&interval=1d&events=history&includeAdjustedClose=true";
+			var requestUri = string.Format (format, symbol, SecondsSinceEpoch (start), SecondsSinceEpoch (end));
 			int retries = 0;
 
+			// GET https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1622906679&period2=1654442679&interval=1d&events=history&includeAdjustedClose=true HTTP/1.1
+			// Host: query1.finance.yahoo.com
+			// User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0
+			// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+			// Accept-Language: en-US,en;q=0.5
+			// Accept-Encoding: gzip, deflate, br
+			// Referer: https://finance.yahoo.com/quote/AAPL/history
+			// Connection: keep-alive
+			// Upgrade-Insecure-Requests: 1
+			// Sec-Fetch-Dest: document
+			// Sec-Fetch-Mode: navigate
+			// Sec-Fetch-Site: same-site
+			// Sec-Fetch-User: ?1
+			// Sec-GPC: 1
+
 			do {
-				using (var response = await client.GetAsync (requestUri, cancellationToken).ConfigureAwait (false)) {
-					using (var stream = await response.Content.ReadAsStreamAsync (cancellationToken).ConfigureAwait (false)) {
-						if (response.IsSuccessStatusCode)
-							return await YahooStockData.LoadAsync (stream, cancellationToken).ConfigureAwait (false);
+				using (var request = new HttpRequestMessage (HttpMethod.Get, requestUri)) {
+					request.Headers.Add ("Accept-Language", "en-US");
+					request.Headers.Add ("Referer", $"https://https://finance.yahoo.com/quote/{symbol}/history");
+					request.Headers.Add ("Connection", "keep-alive");
 
-						if (response.StatusCode == HttpStatusCode.Unauthorized && retries < 5) {
-							await Task.Delay (500).ConfigureAwait (false);
-							retries++;
-							continue;
-						}
+					using (var response = await client.SendAsync (request, cancellationToken).ConfigureAwait (false)) {
+						using (var stream = await response.Content.ReadAsStreamAsync (cancellationToken).ConfigureAwait (false)) {
+							if (response.IsSuccessStatusCode)
+								return await YahooStockData.LoadAsync (stream, cancellationToken).ConfigureAwait (false);
 
-						using (var reader = new StreamReader (stream)) {
-							var text = reader.ReadToEnd ();
-							var json = JObject.Parse (text);
+							if (response.StatusCode == HttpStatusCode.Unauthorized && retries < 5) {
+								await Task.Delay (1000).ConfigureAwait (false);
+								retries++;
+								continue;
+							}
 
-							var code = json.SelectToken ("finance.error.code").ToString ();
-							var description = json.SelectToken ("finance.error.description").ToString ();
+							using (var reader = new StreamReader (stream)) {
+								var text = reader.ReadToEnd ();
+								var json = JObject.Parse (text);
 
-							throw new Exception ($"{code}: {description}");
+								var code = json.SelectToken ("finance.error.code").ToString ();
+								var description = json.SelectToken ("finance.error.description").ToString ();
+
+								throw new Exception ($"{code}: {description}");
+							}
 						}
 					}
 				}
